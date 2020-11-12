7e4cc1e39e3f54be1ff49b43a1fb83caa356ffa3
Taetertot <68134629+Taetertot@users.noreply.github.com>
2020-11-11 22:10:35 -0500

    Databases
    Created keyword and url databases
    Inserted url searches into url database

--- Resultsfromkeywordsearch.py
+++ Resultsfromkeywordsearch.py
@@ -4,9 +4,11 @@ from bs4 import BeautifulSoup
 import re
 import urllib.parse
 from urllib.parse import urlparse
-
-
- #delete the above later
+from VIAA_Database import *
+from tld import get_tld
+import sqlite3
+from sqlite3 import Error
+ 
 def googleSearch(query):
     g_clean = [] #this is the list we store the search results
     url = 'https://www.google.com/search?client=ubuntu&channel=fs&q={}&ie=utf-8&oe=utf-8'.format(query) #this is the actual query we are going to scrape    
@@ -35,7 +37,29 @@ def googleSearch(query):
     finally:
         return g_clean
 
-#just for testintg
+#just for testing
 query = "Puppies"
 url_results = googleSearch(query)
-print(url_results)
+
+database = r"db\VIAA..db"
+conn = create_connection(database)
+
+#Insert into table
+def create_url(conn, website_name, url):
+    """
+    Create a new url into the url table
+    :param conn:
+    :param url:
+    :return: url id
+    """
+    sql = ''' INSERT INTO url(website_name,url)
+              VALUES(?,?) '''
+    cur = conn.cursor()
+    cur.execute(sql, (website_name, url))
+    conn.commit()
+    return cur.lastrowid
+
+for url in url_results:
+    #Get Domian name
+    info = get_tld(url, as_object=True)
+    create_url(conn, info.domain, url)    
---
+++ VIAA_Database.py
@@ -0,0 +1,61 @@
+import sqlite3
+from sqlite3 import Error
+
+
+def create_connection(VIAA_file):
+    """ create a database connection to a SQLite database """
+    conn = None
+    try:
+        conn = sqlite3.connect(VIAA_file)
+        print(sqlite3.version)
+        return conn
+    except Error as e:
+        print(e)
+    
+
+
+
+#Create URL and Keyword tables
+def create_table(conn, create_table_sql):
+    """ create a table from the create_table_sql statement
+    :param conn: Connection object
+    :param create_table_sql: a CREATE TABLE statement
+    :return:
+    """
+    try:
+        c = conn.cursor()
+        c.execute(create_table_sql)
+    except Error as e:
+        print(e)
+
+def main():
+    database = r"db\VIAA..db"
+
+    sql_create_url_table = """ CREATE TABLE IF NOT EXISTS url (
+                                        id integer PRIMARY KEY,
+                                        website_name text NOT NULL,
+                                        url text NOT NULL
+                                    ); """
+
+    sql_create_keyword_table = """CREATE TABLE IF NOT EXISTS keyword (
+                                    id integer PRIMARY KEY,
+                                    user_id text NOT NULL,
+                                    keyword text NOT NULL
+                                );"""
+
+    # create a database connection
+    conn = create_connection(database)
+
+    # create tables
+    if conn is not None:
+        # create url table
+        create_table(conn, sql_create_url_table)
+
+        # create keyword table
+        create_table(conn, sql_create_keyword_table)
+    else:
+        print("Error! cannot create the database connection.")
+
+
+if __name__ == '__main__':
+    main()
