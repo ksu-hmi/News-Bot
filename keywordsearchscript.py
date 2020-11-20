54cead7ab4351e16d2c96bc60e12312c22207eb5
tksolano <69826097+tksolano@users.noreply.github.com>
2020-11-19 22:40:02 -0500

    Keyword Search Code
    I found some code in our class assignments that I think will help us on our keyword search.

--- keywordsearchscript.py
+++ keywordsearchscript.py
@@ -1,33 +1,27 @@
-# Import the class `Flask` from the `flask` module
-from flask import Flask, render_template, request
-# Instantiate a new web application called `app`, with `__name__` representing the current file
-app=Flask(__name__)
-#The@app.route commands define the URLs in the website. 
-#The specific functions directly after@app.route define what happens when those URLs are visited 
-@app.route('/greet', methods=['POST'])
-#The greet() function sets up the response for the submission form in home.html returning the response to home.htm
-def greet():
-    inputName = request.form['myName']
-    ip = request.remote_addr
-    #write data to file or to DB
-    inputName = inputName.upper()+" hi!  Visiting from " + str(ip)
-    return render_template("home.html",myName=inputName)
-@app.route('/')
-#In home(), Python callsFlask’s render_template function, which looks in the “templates” folder for the file mentioned
-#and passes the variable myName to the template as blank by default. The home.html file says
-#it extends layout.html. So, the render_template function goes to layout.html and assembles
-#the html response, inserting home.html as a block named “content” where indicated
-def home():
+# Module 4.3
+# [ ] create Get Name program
+
+while True:
+    familar_name = input("Enter a familar name: \n")
     
-    return render_template("home.html",myName="")
+    if familar_name.isalpha() == True:
+        print("Hi", familar_name, ", glad you could make it!")
+        break
+        
+    else:
+        print("Input has to be one word - no spaces\n")
 
-@app.route('/about/')
-def about():
-    return render_template("about.html")
+# Module 4.2
+# [ ] create and test pre_word()
 
-@app.route('/keyword/')
-def keyword():
-    return render_template("keyword.html")
-
-if __name__=="__main__":
-    app.run(debug=True)
+def pre_word(word):
+    if word.isalpha():
+        if word.startswith('pre'):
+            print("Valid \"pre\" word")
+        else:
+            print("Not a \"pre\" word")
+    else:
+        print("This is not a word")
+    
+testword = input("enter a word that starts with \"pre\": ").lower()
+pre_word(testword)                 
